ab0b3aeaa5abf20aaffe1bb4ec69ec76e4690d4e
= <=>
2023-08-06 18:01:07 -0700

    Add first draft of scene with RefractionMaterial

--- app/natureScene.tsx
+++ app/natureScene.tsx
@@ -1,19 +1,102 @@
 "use client"
 
-import { Canvas } from "@react-three/fiber"
-import { ColorRepresentation } from "three"
+import {
+  AccumulativeShadows,
+  Caustics,
+  CubeCamera,
+  Environment,
+  MeshRefractionMaterial,
+  OrbitControls,
+  RandomizedLight,
+} from "@react-three/drei"
+import { Canvas, useLoader } from "@react-three/fiber"
+import { Perf } from "r3f-perf"
+import { useControls } from "leva"
+import { Texture } from "three"
+import { RGBELoader } from "three-stdlib"
 
-export default function NatureScene({
-  color = 0xffffff,
-}: {
-  color?: ColorRepresentation
-}) {
+function Diamond() {
+  const diamondConfig = useControls("Diamond", {
+    position: [0, 2, 0],
+    causticPosition: [0, -1, 0],
+  })
+  const envTexture = useLoader(
+    RGBELoader,
+    "https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/industrial_sunset_puresky_1k.hdr"
+  )
+
   return (
-    <>
-      <Canvas h-48>
-        <color attach="background" args={[color]} />
-        <ambientLight intensity={0.5} />
-      </Canvas>
-    </>
+    <CubeCamera envMap={envTexture}>
+      {(texture: Texture) => {
+        return (
+          <Caustics
+            causticsOnly={false}
+            backside
+            debug
+            position={diamondConfig.causticPosition}
+            lightSource={[5, 5, -10]}
+          >
+            <mesh castShadow position={diamondConfig.position}>
+              <torusKnotGeometry />
+              <MeshRefractionMaterial envMap={texture} />
+            </mesh>
+          </Caustics>
+        )
+      }}
+    </CubeCamera>
+  )
+}
+
+export default function NatureScene() {
+  // Helpers
+  //   const directionalLightRef = useRef<DirectionalLight>(null!)
+  //   useHelper(directionalLightRef, DirectionalLightHelper, 1, "hotpink")
+
+  // Debug controls
+  const mainConfig = useControls("Main", {
+    directionalLightPosition: [0, 0, 0],
+    directionalLightIntensity: { value: 1, min: 0, max: 5 },
+    ambientLightIntensity: { value: 0.5, min: 0, max: 5 },
+    backgroundColor: "#f0f0f0",
+  })
+
+  return (
+    <Canvas shadows camera={{ position: [-5, 0.5, 5], fov: 45 }}>
+      <Perf />
+
+      <color attach="background" args={[mainConfig.backgroundColor]} />
+      <ambientLight intensity={mainConfig.ambientLightIntensity} />
+
+      <Diamond />
+      <mesh castShadow position={[-2, 0.5, -2]}>
+        <sphereGeometry />
+        <meshStandardMaterial color={"red"} />
+      </mesh>
+      <mesh castShadow position={[2, 0.5, 2]}>
+        <sphereGeometry />
+        <meshStandardMaterial color={"blue"} />
+      </mesh>
+      <AccumulativeShadows
+        temporal
+        frames={100}
+        colorBlend={2}
+        toneMapped={true}
+        alphaTest={0.3}
+        opacity={0.5}
+        scale={12}
+        position={[0, -0.5, 0]}
+      >
+        <RandomizedLight
+          amount={8}
+          radius={10}
+          ambient={0.5}
+          intensity={1.5}
+          position={[5, 5, -10]}
+          bias={0.001}
+        />
+      </AccumulativeShadows>
+      <OrbitControls />
+      <Environment preset="city" />
+    </Canvas>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
-import { Suspense } from "react"
-import NatureScene from "./natureScene"
+import { Suspense } from "react";
+import NatureScene from "./natureScene";
 
 export default function Home() {
   return (
     <div>
       <section className="flex flex-col h-96 w-full">
-        <NatureScene color={0xff0000} />
-      </section>
-      <section className="flex flex-col h-96 w-full">
-        <NatureScene color={0x00ff00} />
-      </section>
-      <section className="flex flex-col h-96 w-full">
-        <NatureScene color={0x0000ff} />
+        <NatureScene />
       </section>
     </div>
-  )
+  );
 }
